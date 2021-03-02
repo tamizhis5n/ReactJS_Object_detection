62a6b7e35e41190ff6a340118a939c5f22952094
Tamizh <tamizh@pop-os.localdomain>
2021-03-02 23:13:07 +0530

    dynamic version v1

---
+++ Dynamic/src/utilities.js
@@ -0,0 +1,20 @@
+export const drawRect=(detections,ctx)=>{
+    detections.forEach(prediction=>{
+        const [x,y,width,height]=prediction['bbox'];
+        const text=prediction['class'];
+
+
+     
+        //const color="#" +Math.floor(Math.random()*16872392).toString(16);
+        const color="yellow"
+        ctx.strokeStyle=color
+        ctx.font="18px Arial"
+        ctx.fillStyle=color
+
+        ctx.beginPath()
+        ctx.fillText(text,x,y)
+        ctx.rect(x,y,width,height)
+        ctx.stroke()
+   
+    })
+}
